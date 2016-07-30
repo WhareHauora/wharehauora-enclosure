8cf30e8a0eaeeca52ddb765eac7f175b1107d5a8
Steve Baker <steve@stevebaker.org>
2016-07-30 14:40:34 +1200

    Completely change screw layout, add board guides
    Also shrink the lid so that it just covers the end
    (bottom)

--- sensor1.py
+++ sensor1.py
@@ -1,5 +1,6 @@
 import solid as s
 import solid.utils as u
+import math
 
 # basic dimensions
 width = 83.0
@@ -9,21 +10,24 @@ wall_width = 2.5
 nt = 0.45  # nestle tolerance, so body and lid fit together
 emboss_depth = 0.5  # logo emboss depth
 
-# bottom of the Uno board to the top of the shield
-# taking no account of the components
-board_height = 15.0
-pins_height = 2.0  # height of component pins off the board
+# board guide dimensions
+boards_height_inner = 10.5  # top of Uno to bottm of shield
+guide_z = 8.0
+guide1_y = 2.5
+guide2_y = 50.8
+guide_depth = 8.0
+guide_width = 3.0
+lid_guide1_z = 6.0
+lid_guide2_z = 20.0
+lid_guide_width = 8.0
+lid_guide_y = 0.0
 
 # dimensions for M3 25mm bolts
 bolt_hole_clear = 3.0
 bolt_hole_tapped = 2.5  # self-tapping into plastic
 bolt_head_dia = 6.0  # 5 plus clearance
-bolt_lid_height = 7.0  # underside of lid to top of shield board
-bolt_dia1 = 10.0
-bolt_dia2 = 5.0
-bolt1_x, bolt1_y = 6.9, 45.7
-bolt2_x, bolt2_y = 56.8, 2.5
-bolt3_x, bolt3_y = 58.1, 50.8
+bolt_from_edge = 5.0
+bolt_base_dia = 10.0
 
 # dimensions for holes
 power_y, power_z = 45.8, 14.5
@@ -34,7 +38,7 @@ usb_width = 12.0
 usb_height = 9.0
 sensor_x, sensor_y = 56.1, 27.0
 sensor_width = 18.0
-sensor_depth = 24.0
+sensor_depth = 26.0
 
 # misc
 segments = 128
@@ -47,11 +51,21 @@ outer_rad = height / 2.0
 inner_rad = outer_rad - wall_width
 inner_height = height - walls_width
 inner_depth = depth - walls_width
-bolt_body_height = inner_height - board_height - bolt_lid_height
 # lid depth goes as far as the end of the sensor hole
-lid_depth = depth - sensor_x + (sensor_depth / 2.0)
+top_hole_depth = depth - sensor_x + (sensor_depth / 2.0)
+half_guide_width = guide_width / 2.0
 
 square_width = width - height  # the width without the round edge
+bolt_z = height / 2.0
+bolt1_y = bolt_from_edge - outer_rad
+bolt2_y = square_width + outer_rad - bolt_from_edge
+bolt_base1_y = (bolt_base_dia / 2.0) - outer_rad
+bolt_base2_y = square_width + outer_rad - (bolt_base_dia / 2.0)
+bolt_base_depth = depth / 2.0
+# rotation of bolt base code to merge into the side
+bolt_base_angle = math.degrees(math.atan2(
+    bolt_base_depth, bolt_base_dia / 2.0))
+print(bolt_base_angle)
 hnt = nt / 2.0  # half nested tolerance
 
 # body centre
@@ -81,20 +95,38 @@ inner += u.forward(square_width)(u.up(inner_rad)(
 # subtract inner from body
 body -= u.right(wall_width)(u.up(wall_width)(inner))
 
-# subtract lid top nestle
-body -= u.right(depth - lid_depth + half_wall_width)(u.up(
-    height - half_wall_width)(
-        s.cube(size=[depth - lid_depth, square_width, half_wall_width])
-))
-body -= u.forward(half_wall_width)(u.right(depth - lid_depth + wall_width)(
-    u.up(height - wall_width)(
-        s.cube(size=[
-            depth - lid_depth - wall_width,
-            square_width - wall_width,
-            half_wall_width
-        ])
+# subtract sensor hole
+body -= u.forward(sensor_y - sensor_width / 2.0)(
+    u.right(depth - top_hole_depth + half_wall_width)(u.up(
+        height - wall_width)(
+            s.cube(size=[depth - top_hole_depth, sensor_width, wall_width])
+    ))
+)
+
+# bolt base 1
+body += u.up(bolt_z)(u.forward(bolt_base1_y)(u.right(depth - half_wall_width)(
+    s.rotate(a=[90 - bolt_base_angle, -90, 0])(
+        s.cylinder(
+            d1=bolt_base_dia, d2=0.0, h=bolt_base_depth, segments=segments)
     )
-))
+)))
+body -= u.up(bolt_z)(u.forward(bolt1_y)(u.right(depth)(
+    s.rotate(a=[0, -90, 0])(
+        s.cylinder(d=bolt_hole_tapped, h=bolt_base_depth, segments=segments)
+    )
+)))
+# bolt base 2
+body += u.up(bolt_z)(u.forward(bolt_base2_y)(u.right(depth - half_wall_width)(
+    s.rotate(a=[bolt_base_angle - 90, -90, 0])(
+        s.cylinder(
+            d1=bolt_base_dia, d2=0.0, h=bolt_base_depth, segments=segments)
+    )
+)))
+body -= u.up(bolt_z)(u.forward(bolt2_y)(u.right(depth)(
+    s.rotate(a=[0, -90, 0])(
+        s.cylinder(d=bolt_hole_tapped, h=bolt_base_depth, segments=segments)
+    )
+)))
 
 # subtract lid side nestle
 body -= u.up(half_wall_width)(u.right(depth - half_wall_width)(
@@ -119,30 +151,27 @@ body -= u.forward(square_width)(u.right(depth - half_wall_width)(
     )
 ))
 
-# lid top
-lid = u.forward(hnt)(u.right(depth - lid_depth + half_wall_width + hnt)(
-    u.up(height - half_wall_width)(
-        s.cube(size=[lid_depth - nt, square_width - nt, half_wall_width])
-    )
+# body guides
+body += u.up(guide_z)(u.forward(guide1_y - wall_width - half_guide_width)(
+    s.cube(size=[guide_depth, wall_width, boards_height_inner])
 ))
-lid += u.forward(half_wall_width + hnt)(u.right(
-    depth - lid_depth + wall_width + hnt)(
-        u.up(height - wall_width)(
-            s.cube(size=[
-                lid_depth - half_wall_width - nt,
-                square_width - wall_width - nt,
-                half_wall_width
-            ])
-        )
-    )
-)
+body += u.up(guide_z)(u.forward(guide1_y + half_guide_width)(
+    s.cube(size=[guide_depth, wall_width, boards_height_inner])
+))
+body += u.up(guide_z)(u.forward(guide2_y - wall_width - half_guide_width)(
+    s.cube(size=[guide_depth, wall_width, boards_height_inner])
+))
+body += u.up(guide_z)(u.forward(guide2_y + half_guide_width)(
+    s.cube(size=[guide_depth, wall_width, boards_height_inner])
+))
+
 
 # lid side nestle
-lid += u.up(half_wall_width + hnt)(u.right(depth + hnt - half_wall_width)(
+lid = u.up(half_wall_width + hnt)(u.right(depth + hnt - half_wall_width)(
     s.cube(size=[
         half_wall_width,
         square_width,
-        height - half_wall_width - nt
+        height - wall_width - nt
     ])
 ))
 lid += u.right(depth + hnt - half_wall_width)(u.up(inner_rad + wall_width)(
@@ -181,66 +210,43 @@ lid += u.forward(square_width)(u.right(depth)(u.up(outer_rad)(
     )
 )))
 
-# lid bolt mount
-bolt_lid = u.up(height - bolt_lid_height - wall_width)(
-    s.cylinder(d1=bolt_dia2, d2=bolt_dia1, h=bolt_lid_height + wall_width,
-               segments=segments) -
-    s.cylinder(d=bolt_hole_tapped, h=bolt_lid_height + 1.0, segments=segments)
-)
-body += u.right(bolt1_x)(u.forward(bolt1_y)(bolt_lid))
-lid += u.right(bolt2_x)(u.forward(bolt2_y)(bolt_lid))
-lid += u.right(bolt3_x)(u.forward(bolt3_y)(bolt_lid))
-
 # lid holes
-power_hole = u.up(power_z)(u.forward(power_y)(u.right(depth)(
+# power hole
+lid -= u.up(power_z)(u.forward(power_y)(u.right(depth)(
     s.cube(size=[wall_width, power_width, power_height], center=True)
 )))
-lid -= power_hole
-usb_hole = u.up(usb_z)(u.forward(usb_y)(u.right(depth)(
+# usb hole
+lid -= u.up(usb_z)(u.forward(usb_y)(u.right(depth)(
     s.cube(size=[wall_width, usb_width, usb_height], center=True)
 )))
-lid -= usb_hole
-sensor_hole = u.up(height)(u.forward(sensor_y)(u.right(sensor_x)(
+# sensor hole
+lid -= u.up(height)(u.forward(sensor_y)(u.right(sensor_x)(
     s.cube(size=[sensor_depth, sensor_width, walls_width], center=True)
 )))
-lid -= sensor_hole
+# bolt hole 1
+lid -= u.up(bolt_z)(u.forward(bolt1_y)(u.right(depth + half_wall_width)(
+    s.rotate(a=[0, -90, 0])(
+        s.cylinder(d=bolt_hole_clear, h=wall_width, segments=segments)
+    )
+)))
+# bolt hole 2
+lid -= u.up(bolt_z)(u.forward(bolt2_y)(u.right(depth + half_wall_width)(
+    s.rotate(a=[0, -90, 0])(
+        s.cylinder(d=bolt_hole_clear, h=wall_width, segments=segments)
+    )
+)))
 
-# body subtraction for lid bolt mount
-bolt_lid = u.up(height - bolt_lid_height - wall_width - nt)(
-    s.cylinder(d1=bolt_dia2 + hnt, d2=bolt_dia1 + hnt,
-               h=bolt_lid_height + wall_width + nt,
-               segments=segments)
-)
-body -= u.right(bolt2_x)(u.forward(bolt2_y)(bolt_lid))
-body -= u.right(bolt3_x)(u.forward(bolt3_y)(bolt_lid))
-
-# body bolt mount
-bolt_body = u.up(wall_width)(
-    s.cylinder(d1=bolt_dia1, d2=bolt_dia2, h=bolt_body_height,
-               segments=segments)
-)
-body += u.right(bolt1_x)(u.forward(bolt1_y)(bolt_body))
-body += u.right(bolt2_x)(u.forward(bolt2_y)(bolt_body))
-body += u.right(bolt3_x)(u.forward(bolt3_y)(bolt_body))
-
-# bolt body hole
-bolt_body_hole = (
-    s.cylinder(d=bolt_head_dia, h=wall_width, segments=segments) +
-    s.cylinder(d=bolt_hole_clear, h=wall_width + bolt_body_height,
-               segments=segments)
-)
-body -= u.right(bolt1_x)(u.forward(bolt1_y)(bolt_body_hole))
-body -= u.right(bolt2_x)(u.forward(bolt2_y)(bolt_body_hole))
-body -= u.right(bolt3_x)(u.forward(bolt3_y)(bolt_body_hole))
-
-# pins next to bolt 2
-pins_hole = u.up(wall_width + bolt_body_height - pins_height)(
-    u.forward(bolt2_y)(u.right(bolt2_x - 3.0)(
-        s.cylinder(d=3, h=pins_height + board_height + pins_height)
-    ))
-)
-body -= pins_hole
-lid -= pins_hole
+# lid guides
+lid += u.up(lid_guide1_z - wall_width)(u.forward(lid_guide_y)(
+    u.right(depth - half_wall_width - guide_depth + hnt)(
+        s.cube(size=[guide_depth, lid_guide_width, wall_width])
+    )
+))
+lid += u.up(lid_guide2_z)(u.forward(lid_guide_y)(
+    u.right(depth - half_wall_width - guide_depth + hnt)(
+        s.cube(size=[guide_depth, lid_guide_width, wall_width])
+    )
+))
 
 logo = u.right(40)(u.up(height - emboss_depth)(
     s.linear_extrude(height=emboss_depth)(
